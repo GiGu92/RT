63a1b987e6d8cd271a4b5ad640d42758be0cce4a
g.szaloki <g.szaloki@gaijin.ru>
2021-04-02 12:39:21 +0200

    6.3-6.7 abstraction, utilities

--- Main.cpp
+++ Main.cpp
@@ -1,31 +1,18 @@
-#include "color.h"
-#include "ray.h"
-#include "vec3.h"
+#include "rtweekend.h"
 
-#include <Shlwapi.h> // for ShellExecute
+#include "color.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
 #include <iostream>
+#include <Shlwapi.h> // for ShellExecute
 
-float hit_sphere(const point3& center, float radius, const ray& r) {
-  vec3 oc = r.origin() - center;
-  float a = r.direction().length_squared();
-  float half_b = dot(oc, r.direction());
-  float c = oc.length_squared() - radius*radius;
-  float discriminant = half_b*half_b - a*c;
-  if (discriminant < 0)
-    return -1.f;
-  else
-    return (-half_b - sqrtf(discriminant)) / a;
-}
-
-color ray_color(const ray& r) {
-  float t = hit_sphere(point3(0, 0, -1), 0.5, r);
-  if (t > 0) {
-    vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
-    return 0.5f * color(N.x()+1, N.y()+1, N.z()+1);
-  }
+color ray_color(const ray& r, const hittable& world) {
+  hit_record rec;
+  if (world.hit(r, 0, infinity, rec))
+    return 0.5f * (rec.normal + color(1, 1, 1));
   vec3 unit_direction = unit_vector(r.direction());
-  t = 0.5f * (unit_direction.y() + 1.0f);
+  float t = 0.5f * (unit_direction.y() + 1.0f);
   return (1.0f - t) * color(1.f, 1.f, 1.f) + t * color(.5f, .7f, 1.f);
 }
 
@@ -35,6 +22,11 @@ int main() {
   const float aspect_ratio = 16.f / 9.f;
   const int image_width = 400;
   const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+  // World
+  hittable_list world;
+  world.add(make_shared<sphere>(point3(0, 0, -1), 0.5f));
+  world.add(make_shared<sphere>(point3(0, -100.5f, -1), 100.f));
 
   // Camera
   float viewport_height = 2.0;
@@ -57,7 +49,7 @@ int main() {
       float u = float(i) / (image_width - 1);
       float v = float(j) / (image_height - 1);
       ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
-      color pixel_color = ray_color(r);
+      color pixel_color = ray_color(r, world);
       write_color(std::cout, pixel_color);
     }
   }
---
+++ sphere.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "hittable.h"
+#include "vec3.h"
+
+class sphere : public hittable {
+public:
+  sphere() : center(0, 0, 0), radius(1) {};
+  sphere(point3 cen, float r) : center(cen), radius(r) {};
+
+  virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const override;
+
+private:
+  point3 center;
+  float radius;
+};
+
+bool sphere::hit(const ray& r, float t_min, float t_max, hit_record& rec) const {
+  vec3 oc = r.origin() - center;
+  float a = r.direction().length_squared();
+  float half_b = dot(oc, r.direction());
+  float c = oc.length_squared() - radius * radius;
+
+  float discriminant = half_b * half_b - a * c;
+  if (discriminant < 0)
+    return false;
+  float sqrtd = sqrtf(discriminant);
+  
+  // Find the nearest root that lies in the acceptable range.
+  float root = (-half_b - sqrtd) / a;
+  if (root < t_min || t_max < root) {
+    root = (-half_b + sqrtd) / a;
+    if (root < t_min || t_max < root)
+      return false;
+  }
+
+  rec.t = root;
+  rec.p = r.at(rec.t);
+  vec3 outward_normal = (rec.p - center) / radius;
+  rec.set_face_normal(r, outward_normal);
+
+  return true;
+}
