83da296b440f7d51c5d5ff594ebd8eb28f7b17eb
Gabor Szaloki <g.szaloki@gaijin.team>
2021-05-03 18:23:45 +0100

    simple scene, perf display at the end

--- Main.cpp
+++ Main.cpp
@@ -7,15 +7,20 @@
 #include "material.h"
 
 #include <iostream>
+#include <chrono>
 #include <Shlwapi.h> // for ShellExecute
 
+static int num_rays = 0;
+
 color ray_color(const ray& r, const hittable& world, int depth) {
-  hit_record rec;
 
   // If we've exceeded the ray bounce limit, no more light is gathered.
   if (depth <= 0)
     return color(0, 0, 0);
 
+  num_rays++;
+
+  hit_record rec;
   if (world.hit(r, 0.001, infinity, rec)) {
     ray scattered;
     color attenuation;
@@ -28,9 +33,7 @@ color ray_color(const ray& r, const hittable& world, int depth) {
   return (1.0 - t) * color(1, 1, 1) + t * color(.5, .7, 1.);
 }
 
-hittable_list random_scene() {
-  hittable_list world;
-
+void random_scene(hittable_list& world) {
   auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
   world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
 
@@ -47,13 +50,15 @@ hittable_list random_scene() {
           color albedo = color::random() * color::random();
           sphere_material = make_shared<lambertian>(albedo);
           world.add(make_shared<sphere>(center, 0.2, sphere_material));
-        } else if (choose_mat < 0.95) {
+        }
+        else if (choose_mat < 0.95) {
           // metal
           color albedo = color::random(0.5, 1);
           real fuzz = random_real(0, 0.5);
           sphere_material = make_shared<metal>(albedo, fuzz);
           world.add(make_shared<sphere>(center, 0.2, sphere_material));
-        } else {
+        }
+        else {
           // glass
           sphere_material = make_shared<dielectric>(1.5);
           world.add(make_shared<sphere>(center, 0.2, sphere_material));
@@ -70,8 +75,19 @@ hittable_list random_scene() {
 
   auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
   world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+}
 
-  return world;
+void simple_scene(hittable_list& world) {
+  auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+  auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+  auto material_left = make_shared<dielectric>(1.5);
+  auto material_right = make_shared<metal>(color(0.7, 0.6, 0.2), 0.0);
+
+  world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
+  world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
+  world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+  world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.4, material_left));
+  world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 }
 
 int main() {
@@ -86,20 +102,9 @@ int main() {
 
   // World
 
-  hittable_list world = random_scene();
-
-  /*
-  auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-  auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-  auto material_left = make_shared<dielectric>(1.5);
-  auto material_right = make_shared<metal>(color(0.7, 0.6, 0.2), 0.0);
-
-  world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
-  world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
-  world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
-  world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),  -0.4, material_left));
-  world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
-  */
+  hittable_list world;
+  //random_scene(world);
+  simple_scene(world);
 
   // Camera
 
@@ -109,15 +114,20 @@ int main() {
   real dist_to_focus = 10;
   real aperture = 0.1;
   camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
-  
+
   // Render
 
   std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
+  auto print_progress = [](int done, int all) {
+    int percent = (int)round((done / real(all)) * 100);
+    std::cerr << "\rScanlines: " << done << "/" << all << " (" << percent << "%)" << std::flush;
+  };
+
+  auto start_time = std::chrono::high_resolution_clock::now();
+
   for (int j = image_height - 1; j >= 0; --j) {
-    int line = image_height - 1 - j;
-    int percent = (int)round((line / real(image_height)) * 100);
-    std::cerr << "\rScanlines: " << line << "/" << image_height << " (" << percent << "%)" << std::flush;
+    print_progress(image_height - 1 - j, image_height);
     for (int i = 0; i < image_width; ++i) {
       color pixel_color(0, 0, 0);
       for (int s = 0; s < samples_per_pixel; ++s) {
@@ -130,8 +140,15 @@ int main() {
       write_color(std::cout, pixel_color, samples_per_pixel);
     }
   }
+  print_progress(image_height, image_height);
 
-  std::cerr << "\nDone.\n";
+  auto finish_time = std::chrono::high_resolution_clock::now();
+  real seconds = (finish_time - start_time).count() * 1e-9;
+
+  std::cerr << "\nDone " << num_rays << " rays in " << seconds << " seconds."
+    << " (" << num_rays * 1e-6 / seconds << " Mray/s)";
+
+  getchar();
 
   ShellExecute(0, 0, L"image.ppm", 0, 0, SW_SHOW);
 };
